1bf28c88dd225afd85580bb8376a5d33c5175c32
Alexey Kiselev <lekha.a-kisel@yandex.ru>
2023-09-15 09:38:46 +0300

    Initial Commit

---
+++ aikiselev_1PW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aikiselev_1PW1
+//
+//  Created by Alexey Kiselev on 15.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
